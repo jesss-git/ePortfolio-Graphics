5db8fc65587c5b20637cf75b06418aba26b57403
jesss-git <jessicachan@Jessicas-MacBook-Pro.local>
2025-11-01 22:34:25 -0400

    Update Proj 2

--- data/Graphics.jsx
+++ data/Graphics.jsx
@@ -19,6 +19,30 @@ import rough2 from "../assets/Images/FINE228-TypeAndForm/Rough2.jpg";
 import rough3 from "../assets/Images/FINE228-TypeAndForm/Rough3.jpg";
 import rough4 from "../assets/Images/FINE228-TypeAndForm/Rough4.jpg";
 
+import pattern2 from "../assets/Images/FINE228-PatternRepeat/pattern2.png";
+import pattern1 from "../assets/Images/FINE228-PatternRepeat/pattern1.png";
+import sample1 from "../assets/Images/FINE228-PatternRepeat/sample1.png";
+import sample2 from "../assets/Images/FINE228-PatternRepeat/sample2.png";
+import sample3 from "../assets/Images/FINE228-PatternRepeat/sample3.png";
+import sample4 from "../assets/Images/FINE228-PatternRepeat/sample4.png";
+import sample5 from "../assets/Images/FINE228-PatternRepeat/sample5.png";
+import sample6 from "../assets/Images/FINE228-PatternRepeat/sample6.png";
+import sample7 from "../assets/Images/FINE228-PatternRepeat/sample7.png";
+import sample8 from "../assets/Images/FINE228-PatternRepeat/sample8.png";
+import geo1 from "../assets/Images/FINE228-PatternRepeat/geo1.png";
+import geo2 from "../assets/Images/FINE228-PatternRepeat/geo2.png";
+import geo3 from "../assets/Images/FINE228-PatternRepeat/geo3.png";
+import geo4 from "../assets/Images/FINE228-PatternRepeat/geo4.png";
+import geo5 from "../assets/Images/FINE228-PatternRepeat/geo5.png";
+import geo6 from "../assets/Images/FINE228-PatternRepeat/geo6.png";
+import geo7 from "../assets/Images/FINE228-PatternRepeat/geo7.png";
+import geo8 from "../assets/Images/FINE228-PatternRepeat/geo8.png";
+import geo9 from "../assets/Images/FINE228-PatternRepeat/geo9.png"; 
+import geo10 from "../assets/Images/FINE228-PatternRepeat/geo10.png";
+import geo11 from "../assets/Images/FINE228-PatternRepeat/geo11.png";
+import geo12 from "../assets/Images/FINE228-PatternRepeat/geo12.png"; 
+import geo13 from "../assets/Images/FINE228-PatternRepeat/geo13.png";
+
 export default function Graphics() {
   const GraphicsProjects = [
     {
@@ -230,11 +254,150 @@ export default function Graphics() {
     },
     {
       id: 2,
-      image: typeAndFormFinal,
-      title: "Graphics Prroj 2",
-      date: "April 2025",
-      description: "Exploring computational sound, I wanted to visualize sound, creating a looper pedal effect where users can record themselves and play back audio, while observing their audio being transformed into a kaleidoscope visual.",
-      tags: ["Python"],
+      image: pattern2,
+      title: "Pattern and Repeat",
+      date: "October 2023",
+      description: "Exploring patterns in art.",
+      tags: ["Illustrator"],
+      sections: [
+        {
+          images: [
+            pattern1,
+          ],
+        },
+        {
+          heading: "Artist Statement",
+          text: [
+            "This piece is to experiment with different technological machines and to display the obvious and often overseen patterns in nature. Although the piece can look quite busy, the repetition of the pattern allows users to feel how busy the bees are, flying from flower to flower, collecting nectar to bring to their hives. I decided to etch the honeycombs because often, when we interact with bees, we see bees racing from flower to flower, but do not typically see their hives. However, when we often think of bees and patterns, their honeycombs are often the first thing that subconsciously comes to mind. In addition to this, etching also created some very interesting shadows and in some scenarios, allowed the honeycombs to have 'depth' and a three-dimensional feel, like real honeycombs.",
+            "I wanted the focus of the piece to be the bees, drawing in the users' eyes to the repetition of the bees with their eye-catching yellow colour. Following that, I wanted the users' eyes to eventually be drawn to the 'oddly coloured' flowers that surround the bees in what feels like the background. I chose this particular vinyl for the flowers because I wanted to try and imitate how bees see them, seeing UV colours and being pulled towards the centre of the flower where the nectar would be. Finally, I decided to etch honeycombs into the background because it created about a 10%-20% opacity, which resembles how bees cannot see the honeycombs within their hives but use their other senses to navigate and find their way around the hives instead.", 
+          ],
+        },
+        {
+          heading: "Experimentation and Process Work",
+        },
+        {
+          heading: "Introduced to Project",
+          text: [
+            "To begin, I thoroughly read over the document for the assignment, Patterns and Repeat, to try and gain a better understanding of the project and what we are expected to do. I also went through examples provided in the document to try and get inspiraiton. When reading through the project guidelines, I got really intimidated when it mentioned that we needed to use and explore different digital technologies available to us that we have not encountered or used before.",
+            "The idea of creating patterns really made me intrigued because it is subconsciously everywhere around us, whether it is seen in interior design, nature, or hobbies. Especially since I love crocheting and creating bracelets, and both require much pattern and repetition, I thought it would be interesting to try and maybe explore those patterns and create them into a 2D project. Also, because I often work with my hands, as well as yarn or thread, I thought the embroidery machine would be something I would be interested in working with for this project.",
+          ],
+        },
+        {
+          heading: "Considering Machines",
+        },
+        {
+          heading: "Introduction to Available Machines",
+          text: [
+            "When I went to class, Tim introduced varous machines to me and a couple other students. Some machines that he showed us included the 3D printer, embroidery machine, laser cutter, and vinyl machine. All these machines sounded very intriguing and seemed like they could create very interesting effects for our project.",
+          ],
+        },
+        {
+          heading: "3D Printer",
+          text: [
+            "The 3D printer seemed like a very interesting machine to use, especially because I have never used one before. I have heard much about the technology, but never got the opportunity to explore it myself. I thought it would be extremely cool to create a 3D object with various faces, each face having a unique and individual pattern to it. However, this might be extremely difficult to do. Although Tim suggested a few software for creating 3D models, I was very curious if Blender would have been possible to create such 3D models that are appropriate for the machine. In the end, one reason I began to stray away from this option / machine was because the material it used was expensive and the colours offered can be limited.",
+          ],
+        },
+        {
+          heading: "Embroidery Machine",
+          text: [
+            "As previously stated, when I first read about the embroidery machine in the project description, I was very interested in it. Especially since I work a lot with yarn and threads during my spare time, I know that such material can create very different effects and designs, depending on the specific technique and colours used. So initially, I was very interested about using the embroidery machine to try and experiment with different stitch techniques and see how it impacted the pattern / design. However, when Tim was introducing the machines to us, he mentioned that it is one of the most difficult / hardest machines of all the options to work with. He said that sometimes the projects do not turn out to look as nice as he was expecting. Due to the uncertainty that could be produced by the embroidery machine, I have chosen to avoid it for this project.",
+          ],
+        },
+        {
+          heading: "Laser Cutter",
+          text: [
+            "When Tim showed us some things created with the laser cutter, I was extremely intrigued. I learned that the laser cutter is mostly used with natural objects, such as wood. Although the laser cutter creates black and white like designs on wood by burning into it, I thought the effect was extremely interesting. Especially with different materials (such as different pieces of wood), the laser cutter would create different looks / shades when burning into the material. He also showed us an example of a “gradient” created by the laser cutter on wood and thought it was a very cool effect. Since every piece of wood has their own natural patterns within, using the laser cutter to create various depths can help show those natural markings, which could produce a different effect on the final project / design. Tim also showed us that you could use different formats to create the different “gradients”, which could also impact the final feel of the design. For gradients to work properly and show up well / distinctly, Tim suggested we used photoshop and use the scale 0 to 60 and only work in black and white. So, one thing this machine prevents is the use of colour because everything will be referenced based on a black and white design and on one piece of wood.",
+          ],
+        },
+        {
+          heading: "Vinyl Machine",
+          text: [
+            "Initially, when Tim showed us the vinyl machine, I thought it might have been one of the more boring machines to work with. It somewhat reminded me of the last project because the vinyl machine mostly just created cuts in special paper that was sticky on one side only. However, Tim, then, showed us a piece he was experimenting with, which used glass and etching cream. While looking at it, I was so intrigued. The glass was see through, but where the etching cream was used, it took on an opaquer look and had a very interesting texture. I was also thinking it could have been interesting to try and layer different vinyl cut outs to create more interesting visual effects. With both ideas in mind, I started wondering if I could even combine both, where the etching cream was on the very top layer of the glass, and the vinyl cut outs were layered behind. I thought it would have been interesting to see the contrast between the etching cream, transparency of the glass, and the vibrant colours of the cut outs all working together. I was also considering separating the vinyl cut out layers by placing glass between them to create more depth to the project and design.",
+          ],
+        },
+        {
+          heading: "Experiments",
+        },
+        {
+          heading: "Simple Patterns",
+          text: [
+            "▸ First, I began by exploring very simple patterns.",
+            "▸ These initially started off with only working with straight lines, then moving toward curved lines, and finally, trying to combine different things.",
+            "▸ Here, I experimented with different placements of lines, the orientation of the lines, spacing of lines, thickness of lines and colour inversions.",
+            "▸ Through this exploration, I also found that Illustrator has some cool repetition effects by creating swatches.",
+            "▸ I was able to play around with the layout of the repeated pattern.",
+            "▸ For instance, I was able to choose whether I wanted to lay out as boxes (squares) normally, staggered by column, or staggered by row, or if I wanted to layout the pattern as a hexagon, staggered by column or staggered by row.",
+            "▸ In addition to this, I also found out that you can control which parts of the pattern overlap the other, which can create other very cool effects.",
+          ],
+          images: [
+            sample1,
+            sample2,
+            sample3,
+            sample4,
+            sample5,
+            sample6,
+            sample7,
+            sample8,
+          ],
+        },
+        {
+          heading: "Geometric Patterns",
+          images: [
+            geo1,
+            geo2,
+            geo3,
+            geo4,
+            geo5,
+            geo6,
+            geo7,
+            geo8,
+            geo9,
+            geo10,
+            geo11,
+            geo12,
+            geo13,
+          ],
+        },
+        {
+          heading: "",
+          text: [],
+        },
+        {
+          heading: "",
+          text: [],
+        },
+        {
+          heading: "",
+          text: [],
+        },
+        {
+          heading: "",
+          text: [],
+        },
+        {
+          heading: "",
+          text: [],
+        },
+        {
+          heading: "",
+          text: [],
+        },
+        {
+          heading: "",
+          text: [
+            "▸ ",
+            "▸ ",
+            "▸ ",
+            "▸ ",
+            "▸ ",
+            "▸ ",
+            "▸ ",
+            "▸ ",
+            "▸ ",
+            "▸ ",
+          ],
+        },
+      ],
     },
     {
       id: 3,
